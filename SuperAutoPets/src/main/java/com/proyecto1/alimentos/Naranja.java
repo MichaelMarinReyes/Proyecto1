df74330fe20cde6e56c31f951b4ade06aeba3818
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2022-03-31 05:47:49 -0600

    Tier animales y comida y se muestra en la tienda

--- SuperAutoPets/src/main/java/com/proyecto1/alimentos/Naranja.java
+++
@@ -1,9 +0,0 @@
-package alimentos;
-
-/**
- *
- * @author michael
- */
-public class Naranja {
-    
-}
--- SuperAutoPets/src/main/java/com/proyecto1/reportes/ReporteGeneral.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto1.reportes;
-
-/**
- *
- * @author michael
- */
-public class ReporteGeneral {
-    
-}
